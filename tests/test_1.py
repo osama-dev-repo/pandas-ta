9989ade19fd26975d8047263c18abf8faf52db76
OSAMA <150441064+osama-dev-repo@users.noreply.github.com>
2025-01-11 15:19:17 +0000

    ready to prod

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,8 +1,9 @@
 import pandas as pd
-import pandas_ta as ta
+import pandas_ta_remake as ta
 
 df = pd.DataFrame() # Empty DataFrame
 
 df = df.ta.ticker("aapl")
 
-print(df.head())
+df.ta.ema(leangh=14, append=True)
+print(df.dropna().head())
